965c063a02d1036964bb1aa326db2abfc44bfb6c
Jan Beneš <vekama21@gmail.com>
2018-10-23 22:05:40 +0200

    More improvements (preparation for pre-release version)

--- kontakt.php
+++ kontakt.php
@@ -7,39 +7,39 @@ include './parts/nav.php';
       </header>
       <main>
       <div class="contactboxy">
-        <div class="contactbox">
+        <div class="contactbox-img">
           <picture>
             <source media="(min-width: 800)" srcset="./img/profile.jpg">
             <img src="./img/profile.jpg" alt="obr">
           </picture>
         </div>
-        <div class="contactbox">
+        <div class="contactbox-text">
           <h2>Kontakt</h2>
-        <p>
-          <b>Jan Beneš</b>
-          <br />
-          Palackého 25
-          <br />
-          412 01 Litoměřice
-        </p>
-        <ul>
-          <li>
-            <b>mobil:</b>
-            +420 603 226 369
-          </li>
-          <li>
-            <b>IČ:</b>
-            13348213
-          </li>
-          <li>
-            <b>DIČ:</b>
-            CZ5711020062
-          </li>
-          <li>
-            <b>email:</b>
-            <a href='mailto:elmont@email.cz'>elmont@email.cz</a>
-          </li>
-        </ul>
+          <p>
+            <b>Jan Beneš</b>
+            <br />
+            Palackého 25
+            <br />
+            412 01 Litoměřice
+          </p>
+          <ul>
+            <li>
+              <b>mobil:</b>
+              +420 603 226 369
+            </li>
+            <li>
+              <b>IČ:</b>
+              13348213
+            </li>
+            <li>
+              <b>DIČ:</b>
+              CZ5711020062
+            </li>
+            <li>
+              <b>email:</b>
+              <a href='mailto:elmont@email.cz'>elmont@email.cz</a>
+            </li>
+          </ul>
         </div>
 <?php
 include './parts/foot.php';
--- prace.php
+++ prace.php
@@ -3,11 +3,26 @@ include './parts/head.php';
 include './parts/nav.php';
  ?>
       <header>
-        <h1>???</h1>
+        <h1>Malá ukázka mé práce</h1>
       </header>
       <main>
       <div class="carousell">
-        carousel
+        <div class="imagebox">
+          <img src="" alt="">
+          <p></p>
+        </div>
+        <div class="imagebox">
+          <img src="" alt="">
+          <p></p>
+        </div>
+        <div class="imagebox">
+          <img src="" alt="">
+          <p></p>
+        </div>
+        <div class="imagebox">
+          <img src="" alt="">
+          <p></p>
+        </div>
       </div>
 <?php
 include './parts/foot.php';
